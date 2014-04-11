cdbdf3219ee2e69defbe56c2fd4d535ccbb077fd
anoukv <anouk.visser@student.uva.nl>
2014-04-11 16:34:56 +0200

    commented and cleaned up

--- Ass1/visualizePCD.m
+++ Ass1/visualizePCD.m
@@ -2,8 +2,9 @@ function [ ] = visualizePCD( pointCloudData )
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
 
+% Only keep points that have the z < 2
 pointCloudData = pointCloudData(pointCloudData(:, 3) < 2, :);
-size(pointCloudData)
+
 scatter3(pointCloudData(:, 1), pointCloudData(:, 2), pointCloudData(:, 3))
 
 end
