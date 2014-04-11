34680d5c5512cd21cd191217e63397652dc715c3
RemideZ <remi.de.z@gmail.com>
2014-04-11 15:42:59 +0200

    some dummy code

---
+++ Ass1/main.m
@@ -0,0 +1,31 @@
+
+
+% Load the data:
+data = readPCDData('../../data/');
+
+% Initialize R as identity matrix and t=0
+R;
+
+t = 0;
+
+previousRMS = -1;
+
+for i=1:10
+    % Find the closest points for each point in the base point set (A1) from the target point set (A2) using brute-force approach.
+    
+    
+    % Refine R and t using Singular Value Decomposition (See doc1, Section 2.3, for details).
+    
+    
+    
+    
+    % Calculate the new RMS.
+    currentRMS = RMS();
+    if currentRMS == previousRMS:
+        break;
+    end
+    previousRMS = currentRMS;
+    
+end
+
+
---
+++ Ass1/readPCDData.m
@@ -0,0 +1,7 @@
+function [ output_args ] = readPCDData( input_args )
+%UNTITLED3 Summary of this function goes here
+%   Detailed explanation goes here
+
+
+end
+
