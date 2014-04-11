09fdce9b7f1bffc326af4bc49b89e69de9f87678
RemideZ <remi.de.z@gmail.com>
2014-04-11 15:13:02 +0200

    Initial for ass1

--- Prep/StitchAssignmentIMS/demoStitch.m
+++ Prep/StitchAssignmentIMS/demoStitch.m
@@ -3,35 +3,17 @@
 left = im2single(rgb2gray(imread('left.jpg')));
 right = im2single(rgb2gray(imread('right.jpg')));
 
-n = 18;
+n = 10;
 p = 4;
 
 [ M, t ] = computeAffineTransformation(left, right, n, p, false);
 
-M = inv(M);
+M
 
-T = maketform('affine', [M; t']);
+% M = [ M(1,1), M(1,2); M(2,1) M(2,2) ];
 
-[x, y] = tformfwd(T,[1 size(left,2)], [1 size(left,1)]);
+M
 
-% xdata and ydata are the data points along which the images will be
-% plottet. These are different to their size because the images move
-% relative to each other.
-xdata = [min(1,x(1)) max(size(right,2),x(2))];
-ydata = [min(1,y(1)) max(size(right,1),y(2))];
-
-% Perform transformation T on the left image and plot on new axes.
-leftNew = imtransform(imread('left.jpg'),T,'Xdata',xdata,'YData',ydata);
-% Perform the identity transformation on the right image and plot on new
-% axes.
-rightNew = imtransform(imread('right.jpg'), maketform('affine', [1 0 0; 0 1 0; 0 0 1]),'Xdata',xdata,'YData',ydata); 
-
-subplot(1, 2, 1);
-imshow(max(leftNew,rightNew), []);
-title('Builtin');
-
-% Reverse M again.
-M = inv(M);
 T = maketform('affine', [M; t']);
 [rightNew, XDATA, YDATA] = imtransform(right, T);
 
@@ -57,8 +39,10 @@ rightTranslated = [row2; row1];
 leftResampled = zeros(size(rightTranslated));
 leftResampled(1:size(left,1), 1:size(left,2)) = left;
 
-subplot(1, 2, 2);
+subplot(1, 1, 1);
 imshow(max(leftResampled,rightTranslated), []);
 title('home made');
+
+
 
 clear();
