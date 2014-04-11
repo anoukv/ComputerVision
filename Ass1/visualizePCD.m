21da9023362402e468786021579ed6c2cbcbd2e4
anoukv <anouk.visser@student.uva.nl>
2014-04-11 16:12:26 +0200

    made visualization

---
+++ Ass1/visualizePCD.m
@@ -0,0 +1,7 @@
+function [ ] = visualizePCD( pointCloudData )
+%UNTITLED Summary of this function goes here
+%   Detailed explanation goes here
+scatter3(pointCloudData(:, 1), pointCloudData(:, 2), pointCloudData(:, 3))
+
+end
+
