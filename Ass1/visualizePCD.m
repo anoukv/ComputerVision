436f8adba79f30a27680cb2a7007d26e970a9c92
anoukv <anouk.visser@student.uva.nl>
2014-04-11 16:31:49 +0200

    visualization, filtering noise

--- Ass1/visualizePCD.m
+++ Ass1/visualizePCD.m
@@ -1,6 +1,9 @@
 function [ ] = visualizePCD( pointCloudData )
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
+
+pointCloudData = pointCloudData(pointCloudData(:, 3) < 2, :);
+size(pointCloudData)
 scatter3(pointCloudData(:, 1), pointCloudData(:, 2), pointCloudData(:, 3))
 
 end
