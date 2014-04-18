0904fb448b665ae7d668ff4051a04fd2a0e60da9
anoukv <anouk.visser@student.uva.nl>
2014-04-18 10:55:02 +0200

    added comments

--- Ass1/RMS.m
+++ Ass1/RMS.m
@@ -1,6 +1,12 @@
 function [ rms ] = RMS( base, target )
+% something to think about
+% target should be the points that are mapped to the same
+% row as the corresponding point in base
 
+% first find all the square norms form the base and target
 norms = sum(power((base' - target'), 2));
+
+% take the mean of the norms, and take the sqrt
 rms = sqrt(mean(norms));
 
 
