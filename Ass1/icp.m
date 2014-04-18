ea87d293c0ca126d1ed6196b6feda6cde6d11448
RemideZ <remi.de.z@gmail.com>
2014-04-18 12:23:22 +0200

    icp

--- Ass1/icp.m
+++ Ass1/icp.m
@@ -8,14 +8,22 @@ t = zeros(3, 1);
 for i=1:iterations
     % find closest point for each point in base
     % to point in target
-    estimated_target = R * base - t;
+    
+    estimated_target = R * base;
+    estimated_target = estimated_target - repmat(t,1,size(base,2));
+    
+    size(estimated_target)
     kdtree = vl_kdtreebuild(estimated_target);
     
-    closest_points = zeros(size(estimated_target));
-    for j=1:size(estimated_target,1)
-        [index, ~] = vl_kdtreequery(kdtree, estimated_target, target(i,:));
+    closest_points = zeros(size(base));
+    distance = 0;
+    for j=1:size(base,1)
+        tar = target(:,i)
+        [index, dist] = vl_kdtreequery(kdtree, estimated_target, tar);
         closest_points(i,:) = estimated_target(index,:);
+        distance = distance + dist;
     end
+    distance
     dist = RMS(estimated_target, closest_points)
     
     % refine R and t through SVD
