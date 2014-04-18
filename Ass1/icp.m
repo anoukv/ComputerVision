e64c44ef8bfa0552a884e37eff97ed7146958433
RemideZ <remi.de.z@gmail.com>
2014-04-18 11:14:14 +0200

    icp

--- Ass1/icp.m
+++ Ass1/icp.m
@@ -6,9 +6,17 @@ t = zeros(3, 1);
 
 % iterate (iterations) times
 for i=1:iterations
-
     % find closest point for each point in base
     % to point in target
+    estimated_target = R * base - t;
+    kdtree = vl_kdtreebuild(estimated_target);
+    
+    closest_points = zeros(size(estimated_target));
+    for j=1:size(estimated_target,1)
+        [index, ~] = vl_kdtreequery(kdtree, estimated_target, target(i,:));
+        closest_points(i,:) = estimated_target(index,:);
+    end
+    dist = RMS(estimated_target, closest_points)
     
     % refine R and t through SVD
     
--- Ass1/main.m
+++ Ass1/main.m
@@ -1,33 +1,4 @@
 
-
-% Load the data:
-data = readPCDData('../../data/');
-size(data)
-%data
-
-% Initialize R as identity matrix and t=0
-R;
-
-t = 0;
-
-previousRMS = -1;
-
-for i=1:10
-    % Find the closest points for each point in the base point set (A1) from the target point set (A2) using brute-force approach.
-    
-    
-    % Refine R and t using Singular Value Decomposition (See doc1, Section 2.3, for details).
-    
-    
-    
-    
-    % Calculate the new RMS.
-    currentRMS = RMS();
-    if currentRMS == previousRMS;
-        break;
-    end
-    previousRMS = currentRMS;
-    
-end
-
-
+% 
+% data1 = readPcd('/Users/Remi/Desktop/Master/Semester2/data/0000000000.pcd');
+% data1 = readPcd('/Users/Remi/Desktop/Master/Semester2/data/0000000000.pcd');
