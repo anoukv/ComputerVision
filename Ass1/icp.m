3d2b13fdf10cebdd49f1e916b33aabad007f2b0a
anoukv <anouk.visser@student.uva.nl>
2014-04-18 10:57:57 +0200

    lcomments for implementing icp

--- Ass1/icp.m
+++ Ass1/icp.m
@@ -1,7 +1,19 @@
-function [ R, t ] = icp( base, target )
+function [ R, t ] = icp( base, target, iterations )
 
+% initialize Rotation and translation
 R = eye(3, 3);
 t = zeros(3, 1);
+
+% iterate (iterations) times
+for i=1:iterations
+
+    % find closest point for each point in base
+    % to point in target
+    
+    % refine R and t through SVD
+    
+end
+
 
 end
 
