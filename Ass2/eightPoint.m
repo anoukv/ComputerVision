40db56cc67fa69f036f9596dfe027d60ce9f346e
anoukv <anouk.visser@student.uva.nl>
2014-05-02 16:30:47 +0200

    not much change in F after the nonsingular correction

--- Ass2/eightPoint.m
+++ Ass2/eightPoint.m
@@ -4,11 +4,20 @@ n = size(coords1, 2);
 
 A = [coords1(1, :)' .* coords2(1, :)', coords1(1, :)' .* coords2(2, :)', coords1(1, :)', coords1(2, :)' .* coords2(1, :)', coords1(2, :)' .* coords2(2, :)', coords1(2, :)', coords2(1, :)', coords2(2, :)', ones(n, 1)];
 
-[~, E, V] = svd(A);
+%size(A)
+
+[U, D, V] = svd(A);
+
 
 V = V(:,end);
 
-F = reshape(V, 3, 3)';
+%averageSolution = sum(A*V) / n
+
+F = reshape(V, 3, 3)'
+
+[Uf, Df, Vf] = svd(F);
+
+F = Uf * diag([Df(1, 1), Df(2, 2), 0]) * Vf'
 
 end
 
--- Ass2/main.m
+++ Ass2/main.m
@@ -1,3 +1,6 @@
-
-[matches1, matches2] = get_matches(read_image(1, true), read_image(2, true));
+clear()
+im1 = read_image(1, true);
+im2 = read_image(2, true);
+[matches1, matches2] = get_matches(im1, im2);
+plotMatches(im1, im2, matches1, matches2);
 F = eightPoint(matches1, matches2);
