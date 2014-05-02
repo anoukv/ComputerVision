3fb4077cb9d1340eb3a4d60f258c4f74035bb2bc
anoukv <anouk.visser@student.uva.nl>
2014-05-02 17:34:24 +0200

    made normalized eight point algorithm

--- Ass2/eightPointNormalized.m
+++ Ass2/eightPointNormalized.m
@@ -3,6 +3,10 @@ function [ F ] = eightPointNormalized( coords1, coords2 )
 % number of matches
 n = size(coords1, 2);
 
+% normalize points
+coords1 = normalizePoints(coords1);
+coords2 = normalizePoints(coords2);
+
 % construct A (not very pretty, better formatting?)
 A = [coords1(1, :)' .* coords2(1, :)', coords1(1, :)' .* coords2(2, :)', coords1(1, :)', coords1(2, :)' .* coords2(1, :)', coords1(2, :)' .* coords2(2, :)', coords1(2, :)', coords2(1, :)', coords2(2, :)', ones(n, 1)];
 
@@ -20,6 +24,20 @@ F = reshape(V, 3, 3)';
 
 % set smallest singular value of D to 0, recompute F
 F = Uf * diag([Df(1, 1), Df(2, 2), 0]) * Vf';
+
+end
+
+function [ normalizedPoints ] = normalizePoints( points )
+mx = mean(points(1, :));
+my = mean(points(2, :));
+mxMatrix = repmat(mx, 1, size(points, 2));
+myMatrix = repmat(my, 1, size(points, 2));
+
+d = mean(sqrt(sum((points - [mxMatrix;myMatrix]).^2)));
+T = [sqrt(2) / d, 0, -mx * sqrt(2) / d; 0, sqrt(2)/d, -my * sqrt(2) / d; 0, 0, 1];
+
+% I needed to add homogeneous coordinates to make the dimensions happy.
+normalizedPoints = T * [points;ones(1, size(points, 2))];
 
 end
 
--- Ass2/main.m
+++ Ass2/main.m
@@ -3,4 +3,6 @@ im1 = read_image(1, true);
 im2 = read_image(2, true);
 [matches1, matches2] = get_matches(im1, im2);
 plotMatches(im1, im2, matches1, matches2);
-F = eightPoint(matches1, matches2);
+FStart = eightPoint(matches1, matches2)
+FNorm = eightPointNormalized(matches1, matches2)
+
