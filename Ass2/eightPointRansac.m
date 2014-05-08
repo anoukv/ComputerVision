da99aa2216074006262402ee6bcfa87302d6679a
anoukv <anouk.visser@student.uva.nl>
2014-05-08 16:17:41 +0200

    finished RANSAC, now testing

--- Ass2/eightPointRansac.m
+++ Ass2/eightPointRansac.m
@@ -1,14 +1,13 @@
 function [ F ] = eightPointRansac( matches1, matches2, numberOfRounds, inlierTrheshold )
 
 % number of matches
-n = size(coords1, 2);
+n = size(matches1, 2);
 
 % normalize points
 [matches1, T] = normalizePoints(matches1);
 [matches2, Tp] = normalizePoints(matches2);
 
-%matches1 = [matches1; ones(1, n)];
-%matches2 = [matches2; ones(1, n)];
+inliers = zeros(1, n);
 
 for i=1:numberOfRounds
     
@@ -35,20 +34,73 @@ for i=1:numberOfRounds
     % set smallest singular value of D to 0, recompute F
     F = Uf * diag([Df(1, 1), Df(2, 2), 0]) * Vf';
     
-    coords1 = [coords1;ones(1, size(coords1, 2))];
-    coords2 = [coords2;ones(1, size(coords2, 2))];
-
-
+    get = zeros(1, n);
+    
+    for index=1:n
+        
+        % make it homogenous
+        pi = [matches1(:, index); 1];
+        pip = [matches2(:, index); 1];
+        
+        numerator = (pip' * F * pi)^2;
+        Fpi = F * pi;
+        Fpip = F' * pip;
+        
+        denominator = Fpi(1)^2 + Fpi(2)^2 + Fpip(1)^2 + Fpip(2)^2;
+        
+        d = numerator / denominator;
+        if d < inlierThreshold:
+            get(index) = 1;
+        end
+        
+    end
+    
+    if sum(get) > sum(inliers):
+        inliers = get;
+    end
+    
 end
 
-% denormalization
+indices = zeros(1, sum(inliers));
+indexer = 1;
+for i=1:n:
+    if get(i) == 1:
+        indices(indexer) = i;
+        indexer = indexer + 1;
+    end
+end
+
+coords1 = matches1(:, indices);
+coords2 = matches2(:, indices);
+
+% construct A (not very pretty, better formatting?)
+A = [coords1(1, :)' .* coords2(1, :)', coords1(1, :)' .* coords2(2, :)', coords1(1, :)', coords1(2, :)' .* coords2(1, :)', coords1(2, :)' .* coords2(2, :)', coords1(2, :)', coords2(1, :)', coords2(2, :)', ones(n, 1)];
+
+% do SVD, get V
+[~, ~, V] = svd(A);
+
+% take the last column of V (corresponds to smallest singular value)
+V = V(:,end);
+
+% reshape that column to get F
+F = reshape(V, 3, 3)';
+
+% do SVD on F
+[Uf, Df, Vf] = svd(F);
+
+% set smallest singular value of D to 0, recompute F
+F = Uf * diag([Df(1, 1), Df(2, 2), 0]) * Vf';
+
+% AND FINALLY denormalization
 F = Tp'*F*T;
     
 end
 
 function [ normalizedPoints, T ] = normalizePoints( points )
+
 mx = mean(points(1, :));
 my = mean(points(2, :));
+
 mxMatrix = repmat(mx, 1, size(points, 2));
 myMatrix = repmat(my, 1, size(points, 2));
 
