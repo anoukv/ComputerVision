8795d3a6b145247f5f7f4a3a34904f594c15b52f
anoukv <anouk.visser@student.uva.nl>
2014-05-09 09:24:06 +0200

    fixed problem with finding SIFTs in teddy bear

--- Ass2/main.m
+++ Ass2/main.m
@@ -1,16 +1,9 @@
 clear()
-im1 = read_image(1, true);
-im2 = read_image(2, true);
+im1 = read_image(1, false);
+im2 = read_image(2, false);
 [matches1, matches2] = get_matches(im1, im2);
-%plotMatches(im1, im2, matches1, matches2);
+plotMatches(im1, im2, matches1, matches2);
 
 FStart = eightPoint(matches1, matches2)
 FNorm = eightPointNormalized(matches1, matches2)
-FRansac = eightPointRansac(matches1, matches2, 300, 0.1)
-
-% [matches1, matches2] = get_matches(read_image(1, true), read_image(25, true));
-% F = eightPoint(matches1, matches2);
-% 
-% FRansac = eightPointRansac(matches1, matches2, 10, 0.001)
-% F = test(matches1, matches2, 10, 0.001)
-
+FRansac = eightPointRansac(matches1, matches2, 300, 0.001)
--- Ass2/read_image.m
+++ Ass2/read_image.m
@@ -1,6 +1,6 @@
 function [ image ] = read_image( index, house )
 
-if true
+if false
     prefix = '/Users/Remi/Desktop/Master/Semester2/CV_data/Ass2/';
 else
     prefix = '/Users/anoukvisser/dev/MATLAB/data2/';
@@ -13,6 +13,7 @@ if house
         number = strcat('000000', int2str(index));
     end
     image_file = strcat(prefix, 'House/frame', number, '.png');
+    image = im2single(imread(image_file));
 else
     if index < 10
         number = strcat('00', int2str(index));
@@ -20,9 +21,10 @@ else
         number = strcat('0', int2str(index));
     end
     image_file = strcat(prefix, 'TeddyBear/obj02_', number, '.png');
+    image = im2single(rgb2gray(imread(image_file)));
 end
 
-image = im2single(imread(image_file));
+%image = im2single(rgb2gray(imread(image_file)));
 
 end
 
