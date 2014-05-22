b6f371514dbe98814dceac9096ea0b49b11b5cbd
anoukv <anouk.visser@student.uva.nl>
2014-05-22 16:55:40 +0200

    worked on factorization

--- Ass3/main.m
+++ Ass3/main.m
@@ -1,4 +1,8 @@
+clear();
 I1 = imresize(im2single(rgb2gray(imread('obj02_001.png'))), 0.25);
 I2 = imresize(im2single(rgb2gray(imread('obj02_002.png'))), 0.25);
 [coords1, coords2] = get_foreground_matches(I1, I2);
-plotMatches(I1, I2, coords1, coords2);
+coords1 = center(coords1);
+coords2 = center(coords2);
+[M, S] = factorizaion(coords1, coords2)
+%plotMatches(I1, I2, coords1, coords2);
