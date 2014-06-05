d6d1f125924956e76c8cd431267fb6068dc7e43d
anoukv <anouk.visser@student.uva.nl>
2014-06-05 17:14:27 +0200

    report init

--- Ass3/anoukFixesThings.m
+++ Ass3/anoukFixesThings.m
@@ -1,3 +1,5 @@
+% maybe we shouldn't have zeros but make a VERY limited selection...? 
+
 house1 = read_image(1, true);
 house2 = read_image(2, true);
 [coords1, coords2] = get_matches(house1, house2);
