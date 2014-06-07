c9a93e57f2f3cf50fe1dc61b2b6750f1c990855a
RemideZ <remi.de.z@gmail.com>
2014-06-07 17:59:16 +0200

    nothing

--- Ass1/getBodyData.m
+++ Ass1/getBodyData.m
@@ -1,14 +1,8 @@
 function [ data1, data2 ] = getBodyData(  )
 
-<<<<<<< HEAD
 if false
     data1 = cleanup(readPcd('/Users/anoukvisser/dev/MATLAB/data1/0000000050.pcd'));
     data2 = cleanup(readPcd('/Users/anoukvisser/dev/MATLAB/data1/0000000059.pcd'));
-=======
-if true
-    data1 = cleanup(readPcd('/Users/anoukvisser/dev/MATLAB/data1/0000000000.pcd'));
-    data2 = cleanup(readPcd('/Users/anoukvisser/dev/MATLAB/data1/0000000010.pcd'));
->>>>>>> 57344d56bcc367b0c57f6c048075d36406a61fa6
 else
     data1 = cleanup(readPcd('/Users/Remi/Desktop/Master/Semester2/CV_data/Ass1/0000000050.pcd'));
     data2 = cleanup(readPcd('/Users/Remi/Desktop/Master/Semester2/CV_data/Ass1/0000000059.pcd'));
